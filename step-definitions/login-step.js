cb9bab5de90df64d87bfaf844749dd1d56cf2f75
Anastasiia <anastasiyamarket@gmail.com>
2022-12-04 21:00:22 +0100

    fixing failed tests

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -5,24 +5,24 @@ const { LoginPage } = require('../page-objects/login-page')
 const loginPage = new LoginPage()
 
 Given('I visit a login page', async function () {                            // should match a text from 'login.feature'
-    loginPage.navigateToLoginScreen
+    await loginPage.navigateToLoginScreen()
 })
 
 When('I fill the login form with valid credentials', async function () {     // #user-name
-    loginPage.submitLoginForm
+    await loginPage.submitLoginForm()
 })
 
 When(/^I fill the login form with "([^"]*)" and "([^"]*)"$/, async function (username, password) {
     // irregular expressions usage - will be substituted with 'Examples' values from the scenario in 'login.feature' file
-    loginPage.submitLoginWithParameters(username, password)
+    await loginPage.submitLoginWithParameters(username, password)
 })
 
 Then('I should see the home page', async function () {
-    loginPage.UserIsLoggedIn
+    await loginPage.UserIsLoggedIn()
 })
 
 Then('I wait for 3 seconds', async function () {
-    loginPage.pause
+    await loginPage.pause()
 })
 
 //  npm run test                     > 'test' - name of the script in 'package.json' file

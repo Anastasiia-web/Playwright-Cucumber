7ce69151348dde2d0e61afa129435608cc5acbc3
Anastasiia <anastasiyamarket@gmail.com>
2022-12-04 20:47:13 +0100

    hooks updates

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -5,11 +5,16 @@ const { LoginPage } = require('../page-objects/login-page')
 const loginPage = new LoginPage()
 
 Given('I visit a login page', async function () {                            // should match a text from 'login.feature'
-    loginPage.submitLoginForm
+    loginPage.navigateToLoginScreen
 })
 
 When('I fill the login form with valid credentials', async function () {     // #user-name
     loginPage.submitLoginForm
+})
+
+When(/^I fill the login form with "([^"]*)" and "([^"]*)"$/, async function (username, password) {
+    // irregular expressions usage - will be substituted with 'Examples' values from the scenario in 'login.feature' file
+    loginPage.submitLoginWithParameters(username, password)
 })
 
 Then('I should see the home page', async function () {
@@ -20,13 +25,5 @@ Then('I wait for 3 seconds', async function () {
     loginPage.pause
 })
 
-When(
-    /^I fill the login form with "([^"]*)" and "([^"]*)"$/, async function (username, password) {
-        // irregular expressions usage - will be substituted with 'Examples' values from the scenario in 'login.feature' file
-        loginPage.submitLoginWithParameters(username, password)
-    })
-
-
-
-// npm run test                     > 'test' - name of the script in 'package.json' file
-// npm run report                   > 'report' - name of the script in 'package.json' file
+//  npm run test                     > 'test' - name of the script in 'package.json' file
+// npm run report                    > 'report' - name of the script in 'package.json' file

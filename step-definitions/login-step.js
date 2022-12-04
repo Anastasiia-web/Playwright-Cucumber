816e1757701e4177d314fc35768ee3095f93cf4f
Anastasiia <anastasiyamarket@gmail.com>
2022-12-04 15:27:45 +0100

    'login-steps'  test is added

--- step-definitions/login-step.js
+++ step-definitions/login-step.js
@@ -4,3 +4,14 @@ Given('I visit a login page', async function () {        // should match a text 
     await page.goto('https://www.saucedemo.com/')
 })
 
+When('I fill the login form with valid credentials', async function () {     // #user-name
+    await page.fill('#user-name', 'standard_user')
+    await page.fill('#password', 'secret_sauce')
+    await page.click('#login-button')
+})
+
+Then('I should see the home page', async function () {
+    await page.waitForSelector('.inventory_item_name')
+})
+
+// npm run test
